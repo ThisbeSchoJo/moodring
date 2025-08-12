65cf29e3ad0cd94fba749c494294fa273a4eca5a
Thisbe Scholfield-Johnson <ts1299@georgetown.edu>
2025-08-11 18:42:13 -0700

    Created Entry model in models.py and migrated

--- server/models.py
+++ server/models.py
@@ -11,3 +11,13 @@ class User(db.Model, SerializerMixin):
     id = db.Column(db.Integer, primary_key = True)
     username = db.Column(db.String, unique = True, nullable = False)
     _password_hash = db.Column(db.String)
+
+class Entry(db.Model, SerializerMixin):
+    __tablename__ = 'entries'
+
+    id = db.Column(db.Integer, primary_key = True)
+    title = db.Column(db.String, nullable = False)
+    content = db.Column(db.String, nullable = False)
+    created_at = db.Column(db.DateTime, default = datetime.now)
+    updated_at = db.Column(db.DateTime, default = datetime.now)
+    
