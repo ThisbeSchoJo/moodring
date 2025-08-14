ab711c9e1338675bf1a75f0e6ccf9309f37c29fc
Thisbe Scholfield-Johnson <ts1299@georgetown.edu>
2025-08-14 11:43:31 -0700

    Added some basic styling to journal entries in journal.css and imported file into Journal.js

--- client/src/components/Journal.js
+++ client/src/components/Journal.js
@@ -1,6 +1,7 @@
 import React, { useState, useEffect } from "react";
 import { Link } from "react-router-dom";
 import axios from "axios";
+import "../styling/journal.css";
 
 const Journal = () => {
   const [entries, setEntries] = useState([]);
